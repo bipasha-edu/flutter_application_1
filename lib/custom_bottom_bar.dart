64efba25afb4b9186cc717eb78ed5e7a52cdd1c1
Bipasha Digital Learning <12bipasha2004@gmail.com>
2026-06-29 20:10:03 +0530

    Update custom_bottom_bar.dart

--- lib/custom_bottom_bar.dart
+++ lib/custom_bottom_bar.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_screen2.dart';
-import 'package:flutter_application_1/profile_screen.dart';
-import 'package:flutter_application_1/search_screen.dart';
-import 'package:flutter_application_1/settings_screen.dart';
 
 class CustomBottomBar extends StatefulWidget {
   const CustomBottomBar({super.key});
@@ -12,102 +8,86 @@ class CustomBottomBar extends StatefulWidget {
 }
 
 class _CustomBottomBarState extends State<CustomBottomBar> {
- 
   int selectedIndex = 0;
-   
-  List screen = [HomeScreen2(), SearchScreen(), SettingsScreen(), ProfileScreen() ];
+
+  List screens = [
+    Center(child: Text("Home Screen")),
+    Center(child: Text("Activity Screen")),
+    Center(child: Text("Stats Screen")),
+    Center(child: Text("Profile Screen")),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text("Custom Bar"),
-      backgroundColor: Colors.indigoAccent,
-    ),
+      appBar: AppBar(
+        title: Text("Custom Bar"),
+        backgroundColor: Colors.indigoAccent,
+      ),
 
-    
-      body: screen[selectedIndex], 
+      body: screens[selectedIndex],
+
       bottomNavigationBar: Container(
-    color: Colors.indigoAccent,
-    height: 60,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
+        color: Colors.indigoAccent,
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
 
-      children: [
+            
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 0;
+                });
+              },
+              child: Icon(
+                Icons.home,
+                color: selectedIndex == 0 ? Colors.white : Colors.black,
+              ),
+            ),
 
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              selectedIndex = 0;
-            });
-              
-            },
-            //);
-         // },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.home),
-              SizedBox(height: 5),
-              Text("Home"),
-            ],
-          ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 1;
+                });
+              },
+              child: Icon(
+                Icons.show_chart,
+                color: selectedIndex == 1 ? Colors.white : Colors.black,
+              ),
+            ),
+
+         
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 2;
+                });
+              },
+              child: Icon(
+                Icons.bar_chart,
+                color: selectedIndex == 2 ? Colors.white : Colors.black,
+              ),
+            ),
+
+            
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 3;
+                });
+              },
+              child: Icon(
+                Icons.person,
+                color: selectedIndex == 3 ? Colors.white : Colors.black,
+              ),
+            ),
+
+          ],
         ),
-
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              selectedIndex = 1;
-            });
-          },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.search),
-              SizedBox(height: 5),
-              Text("Search"),
-            ],
-          ),
-        ),
-
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              selectedIndex = 2;
-            });
-          },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.settings),
-              SizedBox(height: 5),
-              Text("Settings"),
-            ],
-          ),
-        ),
-
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              selectedIndex = 3;
-            });
-          },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.account_circle),
-              SizedBox(height: 5),
-              Text("Profile"),
-            ],
-          ),
-        ),
-
-      ],
-    ),
-  )
-
+      ),
     );
-  
-}
+  }
 }
